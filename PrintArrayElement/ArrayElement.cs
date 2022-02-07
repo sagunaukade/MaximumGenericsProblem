c27868bf1b66885cba19a069cc4eb3a45272d4ec
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-07 14:37:13 +0530

    Adding program to print array using generics

---
+++ PrintArrayElement/ArrayElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayElement
+{
+    internal class ArrayElement
+    {
+        public static void ToPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write(element);
+            }
+
+        }
+        public static void ToDisplay<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write(element);
+
+            }
+        }
+        public static void ToExecute<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.Write(element);
+            }
+
+        }
+         
+
+    }
+
+
+}
