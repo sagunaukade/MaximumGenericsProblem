8a36a8a8059425a463b9141f1f3b4773efaffe82
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-07 14:30:58 +0530

    Adding to program array element

---
+++ PrintArrayElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArray = { 1.11, 2.34, 54.6, 42.3, 64.8 };
+            char[] charArray = { 'a', 'b', 'c', 'd', 'e' };
+            ArrayElement.ToPrint(intArray);
+            ArrayElement.ToPrint(doubleArray);
+            ArrayElement.ToPrint(charArray);
+            Console.ReadLine();
+
+        }
+    }
+}
