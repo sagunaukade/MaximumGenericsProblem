d310cdcb5b1c0ce9fbf081ffe642727a158e3e3f
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-02-07 14:29:02 +0530

    Adding program to array element

---
+++ PrintArrayElement/ArrayElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintArrayElement
+{
+    internal class ArrayElement
+    {
+            public static void ToPrint(int[] inputArray)
+            {
+                foreach (int element in inputArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("-----------------------------");
+
+            }
+            public static void ToPrint(double[] inputArray)
+            {
+                foreach (double element in inputArray)
+                {
+                    Console.WriteLine(element);
+
+                }
+                Console.WriteLine("-------------------------------");
+            }
+            public static void ToPrint(char[] inputArray)
+            {
+                foreach (char element in inputArray)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("---------------------------------");
+
+            }
+            
+        
+    }
+}
+
